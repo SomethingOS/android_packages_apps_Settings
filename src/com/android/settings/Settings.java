13538199ed9e368396ef04f6bf661babd1dbda1c
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Implement SomethingOS Settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -512,6 +512,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class SomethingSettingsActivity extends SettingsActivity {}
 
     /**
      * Activity for MediaControlsSettings
