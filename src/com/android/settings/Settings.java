ce51a314d37f4f689c69e3989dd36d0b1d286fa2
DylanAkp <dylan.akpinar@epitech.eu>
2024-02-20 22:21:14 +0100

    Settings: Rebrand SomethingSettings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -501,7 +501,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
-    public static class PixoidSettingsActivity extends SettingsActivity {}
+    public static class SomethingSettingsActivity extends SettingsActivity {}
 
     /**
      * Activity for MediaControlsSettings
