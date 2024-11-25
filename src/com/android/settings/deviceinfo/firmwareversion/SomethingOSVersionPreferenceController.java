e79bd163d3ab2ad9a4c62c0215de2798a55c7d91
DylanAkp <github@dylanakp.dev>
2024-11-25 12:52:36 +0100

    Settings: Show SomethingOS Version

---
+++ src/com/android/settings/deviceinfo/firmwareversion/SomethingOSVersionPreferenceController.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024 - SomethingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class SomethingOSVersionPreferenceController extends BasePreferenceController {
+
+    private static final String SOMETHINGOS_DISPLAY_VERSION_PROP = "ro.somethingos.version";
+
+    private final Context mContext;
+
+    public SomethingOSVersionPreferenceController(Context context, String key) {
+        super(context, key);
+        mContext = context;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE_UNSEARCHABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return SystemProperties.get(SOMETHINGOS_DISPLAY_VERSION_PROP,
+                mContext.getResources().getString(R.string.device_info_default));
+    }
+}
